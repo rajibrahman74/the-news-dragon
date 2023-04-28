2798a940300e4e840902b8f9509283abd683d0a8
rajibrahman74e <rajiburrahman74@gmail.com>
2023-04-29 00:49:45 +0600

    [ADD]-setup-routes

---
+++ src/layout/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../pages/Shared/Header/Header";
+import Footer from "../pages/Shared/Footer/Footer";
+
+const Main = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p className="text-center">
+        <small>copyright ©️ my project 2023</small>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
